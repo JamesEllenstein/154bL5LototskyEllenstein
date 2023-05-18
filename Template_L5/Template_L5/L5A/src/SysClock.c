ebda81243cf8a94f433c88c7dbfa4ea7dd002c06
james michael ellenstein <jamesellenstein@ad.ece.ucsb.edu>
2023-05-17 22:32:17 -0700

    finished part a

--- Template_L5/Template_L5/L5A/src/SysClock.c
+++ Template_L5/Template_L5/L5A/src/SysClock.c
@@ -1,6 +1,20 @@
 #include "SysClock.h"
 
 void System_Clock_Init(void) {
+	
+	// Enable MSI
+	RCC->CFGR &= RCC_CFGR_SW;
+	RCC->CFGR |= RCC_CFGR_SW_MSI;
+
+	RCC->CR &= ~(RCC_CR_MSIRANGE);
+	RCC->CR |= (RCC_CR_MSIRANGE_2);
+	RCC->CR |= RCC_CR_MSIRGSEL;
+	RCC->CR |= RCC_CR_MSION;
+	while((RCC->CR & RCC_CR_MSIRDY) == 0);
+	
+	// Select MSI as system clock source
+
+	/*
 	// Enable HSI
 	RCC->CR |= RCC_CR_HSION;
 	while((RCC->CR & RCC_CR_HSIRDY) == 0);
@@ -9,4 +23,5 @@ void System_Clock_Init(void) {
 	RCC->CFGR &= ~RCC_CFGR_SW;
 	RCC->CFGR |= RCC_CFGR_SW_HSI;
 	while((RCC->CFGR & RCC_CFGR_SWS) == 0);
+	*/
 }
--- Template_L5/Template_L5/L5A/src/main.c
+++ Template_L5/Template_L5/L5A/src/main.c
@@ -12,18 +12,23 @@
 #include "SysClock.h"
 #include "stm32l476xx.h"
 #include <stdio.h>
-
-int main(void) {
+#include <math.h>
+volatile int voltage;
+ int main(void) {
     // Initialization
     System_Clock_Init(); // Switch System Clock = 16 MHz
-
+	
     ADC_Init();
-
+		LED_Init();
+		TIM2_CH1_Init();
     // [TODO] Initialize PWM
-
+		Green_LED_Toggle();
     while (1) {
         // [TODO] Trigger ADC and get result
-
+			ADC1->CR |= ADC_CR_ADSTART;
+			while ((ADC123_COMMON->CSR & ADC_CSR_EOC_MST) != ADC_CSR_EOC_MST);
+			voltage = ADC1->DR;
+			ccrUpdate(floor(((float)voltage / (float)4096) * 16000));
         // [TODO] LED behavior based on ADC result
     }
 }
